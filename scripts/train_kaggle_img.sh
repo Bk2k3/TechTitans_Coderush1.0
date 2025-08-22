aa0b2ed8d99bbe6ae4d7e295e3e47159e84c2d03
Bk2k3 <bhaveshbharti1234567@gmail.com>
2025-08-23 02:59:46 +0530

    different approach , satisfies problem statement

--- scripts/train_kaggle_img.sh
+++
@@ -1,4 +0,0 @@
-# scripts/train_kaggle_img.sh
-#!/usr/bin/env bash
-set -e
-python -u -m src.kaggle_img.train --config configs/kaggle_img_config.yaml
--- scripts/train_repod_edf.sh
+++
@@ -1,4 +0,0 @@
-# scripts/train_repod_edf.sh
-#!/usr/bin/env bash
-set -e
-python -u -m src.repod_edf.train --config configs/repod_edf_config.yaml
